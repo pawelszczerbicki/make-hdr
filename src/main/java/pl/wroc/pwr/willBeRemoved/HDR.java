db906f18e1fcd02ccbd1eff0f07015d085f9e774
Pawel <szczerbicki.pawel@gmail.com>
2013-11-09 15:14:29 +0100

    Initial commit

---
+++ src/main/java/pl/wroc/pwr/willBeRemoved/HDR.java
@@ -0,0 +1,14 @@
+package pl.wroc.pwr.willBeRemoved;
+
+public class HDR {
+
+	/**
+     * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MainWindow mainwin=new MainWindow();
+		
+	}
+
+}
